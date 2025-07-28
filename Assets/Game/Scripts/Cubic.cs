ff9ce891bd3e883f9c297d48b595c2e2d80c7fa0
Miroslav <mircheuss@yandex.ru>
2025-07-28 20:40:00 +0300

    Fix cubic placing

--- Assets/Game/Scripts/Cubic.cs
+++ Assets/Game/Scripts/Cubic.cs
@@ -6,6 +6,9 @@ namespace Game.Scripts
 {
     public class Cubic : MonoBehaviour, IDraggable
     {
+        [SerializeField] private LayerMask layerMask;
+        [SerializeField] private Transform rayPoint;
+        
         private Transform _floor;
 
         public void StartDrag(Transform floor)
@@ -15,7 +18,33 @@ namespace Game.Scripts
 
         public void EndDrag()
         {
-            transform.DOMoveY(_floor.position.y, 1f).SetEase(Ease.Linear);
+            MoveDown();
+            Raycast2DRay();
+        }
+
+        private void Raycast2DRay()
+        {
+            var result = Physics2D.Raycast(rayPoint.position, Vector2.down, 20f, layerMask);
+            
+            if(result.collider != null)
+            {
+                var halfSize = result.collider.bounds.size / 2;
+                var closestPoint = new Vector2(
+                    result.point.x,
+                    result.point.y + halfSize.y
+                );
+                MoveDown(closestPoint);
+            }
+            else
+            {
+                MoveDown();
+            }
+        }
+
+        private void MoveDown(Vector3 position = default(Vector3))
+        {
+            Vector3 targetPosition = position != default(Vector3) ? position : _floor.position;
+            transform.DOMoveY(targetPosition.y, 1f).SetEase(Ease.Linear);
         }
     }
 }
